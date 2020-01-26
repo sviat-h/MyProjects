c4517aac8e82a5873812997b3d6da38644e0ed20
sviatoslav <svyatoslav.grinik@gmail.com>
2020-01-26 16:35:45 +0100

    master: removed some test code

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,9 +2,7 @@ package Test;
 
 public class Test {
     public static void main(String[] args) {
-        for (int i = 0; i < Integer.MAX_VALUE; i++){
-            new Thread().start();
-        }
+
     }
 }
 
